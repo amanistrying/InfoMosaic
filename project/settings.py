d9ea0fe8954cf7178bebdc58799f304b445a6bfa
Aman Gupta <aupta2002@gmail.com>
2023-07-17 05:35:53 +0530

    perfect image

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'serviceprovider',
         'USER': 'root',
-        'PASSWORD': 'Admin@123',
+        'PASSWORD': 'firefrost',
         'HOST': 'localhost',
         'PORT': '3306',
     }
